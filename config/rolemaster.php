95f26a728ba9c76dd9e0a6aeba814e1a6040e3c4
Ruben del Blanco Rey <rubendelblanco@gmail.com>
2019-09-05 21:39:29 +0200

    feat:create new config file for rolemaster data (#21)

---
+++ config/rolemaster.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Here we go with rolemaster rules and constants!
+ */
+
+return [
+    'stats' => [
+        'Ag' => 'Agilidad',
+        'Co' => 'Constitución',
+        'Me'    => 'Memoria',
+        'Ra'    => 'Razón',
+        'Ad'    => 'Autodisciplina',
+        'Em'    => 'Empatía',
+        'In'    => 'Intuición',
+        'Pr'    => 'Presencia',
+        'Fu'    => 'Fuerza',
+        'Rp'    => 'Rapidez'
+    ],
+];

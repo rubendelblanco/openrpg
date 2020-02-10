550514c416eeed87c6722e72f46f6d2031568bba
vagrant <vagrant@homestead>
2020-02-10 21:51:39 +0000

    feat:first approach to profession bonus and skills seeder

--- database/seeds/BonusProfessionSkillCategoriesSeeder.php
+++ database/seeds/BonusProfessionSkillCategoriesSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class BonusProfessionSkillCategoriesSeeder extends Seeder
 {
@@ -11,28 +12,59 @@ class BonusProfessionSkillCategoriesSeeder extends Seeder
      */
     public function run()
     {
-        $data = [
-            'Bardo' => [
-                'Autoc' => [5,'2/7'],
-                'Comun' => [5,'1/1/1'],
-                'DesFis' => [5,'6/14'],
-                'Infl' => [5,'1/4'],
-                'PerPod' => [5,'3/6']
-            ]
-        ];
-        $professions_map = DB::table('professions')->pluck('code', 'name');
+        $json_professions = json_decode(file_get_contents('database/seeds/fixtures/professions.json'));
 
-        foreach ($data as $prof => $categories) {
-            foreach ($categories as $cat => $bonus) {
-                DB::table('bonus_profession_skill_category')->insert(
-                    [
-                        'skill_category_id' => $cat,
-                        'profession_id' => $professions_map[$prof],
-                        'bonus' => $bonus[0],
-                        'dp' => $bonus[1],
-                    ]
-                );
+        foreach ($json_professions as $profession => $data) {
+            $profession_id = Str::slug($profession);
+            $profession_exists = DB::table('professions')->where('code', '=', $profession_id)->exists();
+
+            if ($profession_exists) {
+                
+                foreach ($data->skill_category_ranges as $dp) {
+                    $query = DB::table('skill_categories')->whereRaw('name like \'%' . $dp->skill_cat . '%\'')->first();
+                    $bonus = 0;
+
+                    foreach ( $data->profession_bonus as $b) {
+                       if ($b->prof === $query->name) {
+                           $bonus = $b->bonus;
+                       }
+                    }
+
+                    $row = [
+                        'skill_category_id' => $query->code,
+                        'bonus' => $bonus,
+                        'profession_id' => $profession_id,
+                        'dp' => $dp->ranges
+                    ];
+                    DB::table('bonus_profession_skill_category')->insert($row);
+                }
+
+                //DB::table('bonus_profession_skill_category')->insert
             }
         }
+
+        /* $data = [
+             'Bardo' => [
+                 'Autoc' => [5,'2/7'],
+                 'Comun' => [5,'1/1/1'],
+                 'DesFis' => [5,'6/14'],
+                 'Infl' => [5,'1/4'],
+                 'PerPod' => [5,'3/6']
+             ]
+         ];
+         $professions_map = DB::table('professions')->pluck('code', 'name');
+
+         foreach ($data as $prof => $categories) {
+             foreach ($categories as $cat => $bonus) {
+                 DB::table('bonus_profession_skill_category')->insert(
+                     [
+                         'skill_category_id' => $cat,
+                         'profession_id' => $professions_map[$prof],
+                         'bonus' => $bonus[0],
+                         'dp' => $bonus[1],
+                     ]
+                 );
+             }
+         } */
     }
 }

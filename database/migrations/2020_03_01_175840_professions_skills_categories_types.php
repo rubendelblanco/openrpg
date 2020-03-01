f8c908920e898e12304a15371eaf68f667fd0848
Ruben del Blanco <rubendelblanco@gmail.com>
2020-03-01 19:29:13 +0100

    chore:add categories skills model and seeder

--- database/migrations/2020_03_01_175840_professions_skills_categories_types.php
+++ database/migrations/2020_03_01_175840_professions_skills_categories_types.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProfessionsSkillsTypes extends Migration
+class ProfessionsSkillsCategoriesTypes extends Migration
 {
     /**
      * Run the migrations.
@@ -13,22 +13,21 @@ class CreateProfessionsSkillsTypes extends Migration
      */
     public function up()
     {
-        Schema::table('professions_skills_types', function (Blueprint $table) {
-            Schema::dropIfExists('professions_skills');
+        Schema::table('professions_skills_categories_types', function (Blueprint $table) {
             $skill_types = config('rolemaster.profession_skill_type');
 
-            Schema::create('professions_skills_types', function (Blueprint $table) use ($skill_types) {
+            Schema::create('professions_skills_categories_types', function (Blueprint $table) use ($skill_types) {
                 $table->increments('id');
                 $table->string('profession_id');
-                $table->string('skill_id');
+                $table->string('category_id');
                 $table->enum('skill_type',$skill_types);
-                $table->integer('skill_set')->nullable();
+                $table->integer('number_of_ranges')->nullable();
 
                 $table->foreign('profession_id')
                     ->references('code')->on('professions')
                     ->onDelete('cascade');
-                $table->foreign('skill_id')
-                    ->references('code')->on('skills')
+                $table->foreign('category_id')
+                    ->references('code')->on('skill_categories')
                     ->onDelete('cascade');
             });
         });
@@ -41,6 +40,6 @@ class CreateProfessionsSkillsTypes extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('professions_skills_types');
+        Schema::dropIfExists('professions_skills_categories_types');
     }
 }
